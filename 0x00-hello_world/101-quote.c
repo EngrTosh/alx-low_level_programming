eb8fb955544eda05c6a2089f3ab1bfea360f028c
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-05-01 08:08:23 +0100

    hotfix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -7,6 +9,7 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, message, strlen(message));
 	return (1);
 }
